3efdc509af2fe014e78ec9bc3e9fb76550f3104c
tJithendrakrishna <JithendraKrishna12345@gmail.com>
2025-08-02 10:48:37 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,10 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Simple validation example
-    if (email === 'admin@example.com' && password === 'password123') {
-      setMessage('signin successful!');
-    } else {
-      setMessage('Invalid credentials.');
-    }
-  };
-
   return (
-    <div className="login-container">
-      <h1>Sign Pages</h1>
-      <form onSubmit={handleLogin}>
-        <div className="form-group">
-          <label>Email:</label>
-          <input 
-            type="email" 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required 
-          />
-        </div>
-        <div className="form-group">
-          <label>Password:</label>
-          <input 
-            type="password" 
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required 
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      {message && <p>{message}</p>}
+    <div className="container">
+      <h1 className="red-text">Welcome KLU</h1>
+      <h1 className="green-text">This is the output of cicd-demo1 project</h1>
     </div>
   );
 }
